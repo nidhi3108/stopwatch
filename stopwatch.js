871d2fcc237d2101112340b29733963e7ef0acaf
nidhi3108 <nidhivr310@gamil.com>
2022-10-01 13:19:34 +0530

    ...

--- stopwatch.js
+++ stopwatch.js
@@ -1,27 +1,51 @@
-var second=document.getElementById("seconds")
-var minutes=document.getElementById("minutes")
+var secondNode=document.getElementById("seconds")
+var minutesNode=document.getElementById("minutes")
+var startNode=document.getElementById("start")
+var lapNode=document.getElementById("lap")
+var lapContainerNode=document.getElementsByClassName("lapContainer")
+var lapBox1TextNode=document.getElementById("lapbox1")
+var lapTime1TextNode=document.getElementById("laptime1")
+var horinzontalLine=document.createElement("hr")
+var createLapText=document.createElement("h4")
+var createTimeText=document.createElement("h2")
+
+
+//global variable
 var totalTime=0;
-var start=document.getElementById("start")
 var istimeron=false;
 var timerid;
-start.addEventListener ("click",function(event){
+
+
+//start and stop watch
+startNode.addEventListener ("click",function(event){
     if(istimeron)
     {
         //stop watch
         clearInterval(timerid)
-        start.innerHTML="start"
+        startNode.innerHTML="start"
         istimeron=false; 
     }
     else{
         //start watch
         timerid=setInterval(function() {
-   
+            
             totalTime++;
-            minutes.innerHTML=parseInt(totalTime/60)
-            second.innerHTML=parseInt(totalTime%60)
-       }, 1000);
-       start.innerHTML="stop "
-       istimeron=true;
+            minutesNode.innerHTML=parseInt(totalTime/60)
+            secondNode.innerHTML=parseInt(totalTime%60)
+        }, 60);
+        startNode.innerHTML="stop "
+        //    start.style.backgroundColor="green";
+        istimeron=true;
     }
     
 })
+
+//after clicking lap  save the current time below lap button
+lapNode.addEventListener("click",showLapTime)
+
+function showLapTime(event){
+    // var currentTimeValueNode=document.getElementsByTagName("label")
+    // console.log(currentTimeValueNode);
+    var value=secondNode.value;
+    console.log(value);
+}
