87ab30f3f2093f931d680b8aef67ffaece79e548
nidhi3108 <nidhivr310@gamil.com>
2022-10-02 13:41:10 +0530

    ..

--- stopwatch.js
+++ stopwatch.js
@@ -4,12 +4,12 @@ var minutesNode=document.getElementById("minutes")
 var startNode=document.getElementById("start")
 var lapNode=document.getElementById("lap")
 var lapContainerNode=document.getElementsByClassName("lapContainer")
-var hrlineNode=document.getElementById("hrline")
-var lapBox1TextNode=document.getElementById("lapbox1")
-var lapTime1TextNode=document.getElementById("laptime1")
-var horinzontalLine=document.createElement("hr")
-var createLapText=document.createElement("p")
-var createTimeText=document.createElement("p")
+// var hrlineNode=document.getElementById("hrline")
+// var lapBox1TextNode=document.getElementById("lapbox1")
+// var lapTime1TextNode=document.getElementById("laptime1")
+// var horinzontalLine=document.createElement("hr")
+// var createLapText=document.createElement("p")
+// var createTimeText=document.createElement("p")
 
 
 //global variable
@@ -55,32 +55,44 @@ function showLapTime(){
     console.log(minuteValue);
     var secondValue=secondNode.innerHTML;
     console.log(secondValue);
-    // console.log(hourValue+':'+minuteValue+':'+secondValue);
     var currentTime=hourValue+':'+minuteValue+':'+secondValue;
     console.log(currentTime);
-    addAllLaps(currentTime);
-   
-    // laps.push(currentTime);
-    // localStorage.setItem("laps",JSON.stringify(laps))
-    saveLaps(currentTime)
+    
+    var hrline=document.createElement("div")
+    hrline.setAttribute("id","hrline")
+    lapContainerNode.appendChild(hrline)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+    // addAllLaps(currentTime);
 }
-function addAllLaps(currentTime)
-{
-    hrlineNode.appendChild(horinzontalLine)
-    lapBox1TextNode.appendChild(createLapText)
-    createLapText.innerHTML="Lap1"
-    lapTime1TextNode.appendChild(createTimeText)
-    createTimeText.innerHTML=`${currentTime}`
-}
-function saveLaps(currentTime){
-    let allLaps=localStorage.getItem("laps")
-    if(allLaps)
-    {
-        allLaps=JSON.parse(allLaps)
-    }
-    else{
-        allLaps=[];
-    }
-    allLaps.push(currentTime)
-    localStorage.setItem("laps",JSON.stringify(allLaps))
-}
+// function addAllLaps(currentTime)
+// {
+//     hrlineNode.appendChild(horinzontalLine)
+//     lapBox1TextNode.appendChild(createLapText)
+//     createLapText.innerHTML="Lap1"
+//     lapTime1TextNode.appendChild(createTimeText)
+//     createTimeText.innerHTML=`${currentTime}`
+// }
